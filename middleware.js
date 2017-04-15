5e0a6799ec5148bb258108418f657b94ae583b15
Gene Fernandez <gdf@comcast.net>
2017-04-15 12:38:33 -0400

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,13 @@
+
+
+
+    var middleware = {requiredAuthentication: function (req, res, next) {
+      console.log('Private route hit');
+      next();
+    },
+    logger: function (req, res, next){
+      console.log (new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+      next();
+    }
+  };
+  module.exports = middleware;
--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 var express =require('express');
 var app = express();
 var portNum = 3000;
+var middleware = require ('./middleware.js');
 
 // app.get('/',function (req, res) {
 //   res.send ('Hello Express!');
 // });
 
-var middleware = {
-  requiredAuthentication: function (req, res, next) {
-    console.log('Private route hit');
-    next();
-  },
-  logger: function (req, res, next){
-    console.log (new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-    next();
-  }
-};
+
 // app.use (middleware.requiredAuthentication);
 
 app.use (middleware.logger);
