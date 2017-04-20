a99636dc1f5e3382a9ba58bb8a77bad90540c3d6
Gene Fernandez <gdf@comcast.net>
2017-04-19 23:32:46 -0400

    Changes for Heroku3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express =require('express');
 var app = express();
-var portNum = $port || 3000;
+var portNum = process.env.PORT || 3000;
 var middleware = require ('./middleware.js');
 
 // app.get('/',function (req, res) {
